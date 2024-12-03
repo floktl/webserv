460b8f7a95b414ec81733f5944cdb242d05ddc22
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-12-03 09:49:20 +0100

    research on conf behaviour

--- src/main.hpp
+++ src/main.hpp
@@ -6,18 +6,10 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 10:35:17 by jeberle           #+#    #+#             */
-/*   Updated: 2024/12/03 09:31:56 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/12/03 09:44:44 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef DEBUG
 #  define DEBUG 1
-# endif
-
-# ifndef CONFIG_OPTS
-#  define CONFIG_OPTS "server,listen,server_name,root,index,error_page,location,client_max_body_size"
-# endif
-
-# ifndef LOCATION_OPTS
-#  define LOCATION_OPTS "methods,return,root,autoindex,default_file,cgi,cgi_param,upload_store,client_max_body_size"
 # endif
