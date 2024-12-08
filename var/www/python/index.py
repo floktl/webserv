a06c1f7c27a8ed3e0056b8cb794ac4eb863dd0ed
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-12-08 17:12:33 +0100

    update of php application and DELETE and POST Requests

--- var/www/python/index.py
+++ var/www/python/index.py
@@ -1,25 +1,25 @@
 if __name__ == "__main__":
-    people = ["Alice", "Bob", "Charlie", "Diana"]
+	people = ["Alice", "Bob", "Charlie", "Diana"]
 
-    print("""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Simple HTML Page</title>
-    </head>
-    <body>
-        <h1>Welcome to a simple Web Page!</h1>
-        <p>This is rendered by python!</p>
-        <ul>
-    """)
+	print("""
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Simple HTML Page</title>
+	</head>
+	<body>
+		<h1>Welcome to a simple Web Page!</h1>
+		<p>This is rendered by python!</p>
+		<ul>
+	""")
 
-    for person in people:
-        print(f"            <li>{person}</li>")
+	for person in people:
+		print(f"            <li>{person}</li>")
 
-    print("""
-        </ul>
-    </body>
-    </html>
-    """)
+	print("""
+		</ul>
+	</body>
+	</html>
+	""")
