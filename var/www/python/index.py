e44fe5f9cd70e1a56d888e5115c47ef7a5ac210a
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-12-08 15:24:22 +0100

    cgi path checks and removal of logs

--- var/www/python/index.py
+++ var/www/python/index.py
@@ -1,15 +1,25 @@
 if __name__ == "__main__":
-	print("""
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Simple HTML Page</title>
-	</head>
-	<body>
-		<h1>Welcome to a simple Web Page!</h1>
-		<p>This is rendered by python and flask!</p>
-	</body>
-	</html>
-	""")
+    people = ["Alice", "Bob", "Charlie", "Diana"]
+
+    print("""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple HTML Page</title>
+    </head>
+    <body>
+        <h1>Welcome to a simple Web Page!</h1>
+        <p>This is rendered by python!</p>
+        <ul>
+    """)
+
+    for person in people:
+        print(f"            <li>{person}</li>")
+
+    print("""
+        </ul>
+    </body>
+    </html>
+    """)
