9ab712d8077d9d98159f1d83e49a08642dda1574
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-12-09 15:36:18 +0100

    python app running clean

--- obj/src/utils/Sanitizer.d
+++
@@ -1,6 +0,0 @@
-obj/src/utils/Sanitizer.o: src/utils/Sanitizer.cpp \
- src/utils/./Sanitizer.hpp src/utils/././../main.hpp \
- src/utils/././Logger.hpp
-src/utils/./Sanitizer.hpp:
-src/utils/././../main.hpp:
-src/utils/././Logger.hpp:
