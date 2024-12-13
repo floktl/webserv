dd0772799c5f2d7a5ab34a8f8e40d263451572f9
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-12-13 08:20:37 +0100

    rework non blocking ports setup

--- src/cgi/CgiHandler.cpp
+++ src/cgi/CgiHandler.cpp
@@ -6,296 +6,287 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 14:42:00 by jeberle           #+#    #+#             */
-/*   Updated: 2024/12/12 09:00:09 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/12/13 07:48:58 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "CgiHandler.hpp"
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstring>
+#include <vector>
+#include <iostream>
+
 
 CgiHandler::CgiHandler(
-	int _client_fd,
-	const Location* _location,
-	const std::string& _filePath,
-	const std::string& _method,
-	const std::string& _requestedPath,
-	const std::string& _requestBody,
-	const std::map<std::string, std::string>& _headersMap,
-	std::set<int> _activeFds,
-	const std::map<int, const ServerBlock*>& _serverBlockConfigs
-)
-	: client_fd(_client_fd),
-	location(_location),
-	filePath(_filePath),
-	method(_method),
-	requestedPath(_requestedPath),
-	requestBody(_requestBody),
-	headersMap(_headersMap),
-	activeFds(std::move(_activeFds))
+    int _client_fd,
+    const Location* _location,
+    const std::string& _filePath,
+    const std::string& _method,
+    const std::string& _requestedPath,
+    const std::string& _requestBody,
+    const std::map<std::string, std::string>& _headersMap,
+    std::set<int> _activeFds,
+    const ServerBlock& _config,
+    const std::map<int, const ServerBlock*>& _serverBlockConfigs
+) : client_fd(_client_fd),
+    location(_location),
+    filePath(_filePath),
+    method(_method),
+    requestedPath(_requestedPath),
+    requestBody(_requestBody),
+    headersMap(_headersMap),
+    activeFds(std::move(_activeFds)),
+    config(_config),
+    errorHandler(_client_fd, _config.errorPages)
 {
-	// Konvertiere _serverBlockConfigs (Pointer) in serverBlockConfigs (Werte)
-	for (const auto& pair : _serverBlockConfigs) {
-		if (pair.second) { // Prüfe, ob der Zeiger gültig ist
-			serverBlockConfigs.emplace(pair.first, *(pair.second));
-		}
-	}
-}
-
-std::string CgiHandler::getFileExtension()
-{
-	size_t pos = filePath.find_last_of(".");
-	if (pos == std::string::npos) {
-		Logger::red("No file extension found, check to executable cgi target");
-		return "";
-	}
-	std::string ext = filePath.substr(pos);
-	return ext;
-}
-
-void CgiHandler::closePipe(int fds[2]) {
-	close(fds[0]);
-	close(fds[1]);
+    for (const auto& pair : _serverBlockConfigs) {
+        if (pair.second) {
+            serverBlockConfigs.emplace(pair.first, *(pair.second));
+        }
+    }
 }
 
 bool CgiHandler::createPipes(int pipefd_out[2], int pipefd_in[2]) {
-	if (pipe(pipefd_out) == -1) {
-		Logger::red("Pipe creation failed (out): " + std::string(strerror(errno)));
-		return false;
-	}
-	if (pipe(pipefd_in) == -1) {
-		Logger::red("Pipe creation failed (in): " + std::string(strerror(errno)));
-		close(pipefd_out[0]);
-		close(pipefd_out[1]);
-		return false;
-	}
-	return true;
+    if (pipe(pipefd_out) == -1) {
+        Logger::red("Pipe creation failed (out): " + std::string(strerror(errno)));
+        return false;
+    }
+    if (pipe(pipefd_in) == -1) {
+        Logger::red("Pipe creation failed (in): " + std::string(strerror(errno)));
+        close(pipefd_out[0]);
+        close(pipefd_out[1]);
+        return false;
+    }
+    return true;
 }
-
-
-void CgiHandler::setupChildEnv(const std::map<std::string, std::string>& cgiParams, std::vector<char*>& env) {
-	for (const auto& param : cgiParams) {
-		std::string envVar = param.first + "=" + param.second;
-		env.push_back(strdup(envVar.c_str()));
-	}
-	env.push_back(nullptr);
-}
-
 
 void CgiHandler::runCgiChild(const std::string& cgiPath, const std::string& scriptPath,
-				int pipefd_out[2], int pipefd_in[2],
-				const std::map<std::string, std::string>& cgiParams)
-{
-	dup2(pipefd_out[1], STDOUT_FILENO);
-	close(pipefd_out[0]);
-	close(pipefd_out[1]);
+                             int pipefd_out[2], int pipefd_in[2],
+                             const std::map<std::string, std::string>& cgiParams) {
+    dup2(pipefd_out[1], STDOUT_FILENO);
+    close(pipefd_out[0]);
+    close(pipefd_out[1]);
 
-	dup2(pipefd_in[0], STDIN_FILENO);
-	close(pipefd_in[0]);
-	close(pipefd_in[1]);
+    dup2(pipefd_in[0], STDIN_FILENO);
+    close(pipefd_in[0]);
+    close(pipefd_in[1]);
 
-	std::vector<char*> env;
-	setupChildEnv(cgiParams, env);
+    std::vector<char*> env;
+    setupChildEnv(cgiParams, env);
 
-	char* args[] = {
-		const_cast<char*>(cgiPath.c_str()),
-		const_cast<char*>(scriptPath.c_str()),
-		nullptr
-	};
-	execve(cgiPath.c_str(), args, env.data());
-	Logger::red("execve failed: " + std::string(strerror(errno)));
-	exit(EXIT_FAILURE);
+    char* args[] = {
+        const_cast<char*>(cgiPath.c_str()),
+        const_cast<char*>(scriptPath.c_str()),
+        nullptr
+    };
+    execve(cgiPath.c_str(), args, env.data());
+    Logger::red("execve failed: " + std::string(strerror(errno)));
+    exit(EXIT_FAILURE);
 }
 
-void CgiHandler::writeRequestBodyIfNeeded(int pipe_in) {
-	if (method == "POST" && !requestBody.empty()) {
-		ssize_t written = write(pipe_in, requestBody.c_str(), requestBody.size());
-		if (written < 0)
-			Logger::red("Error writing to CGI stdin: " + std::string(strerror(errno)));
-	}
-	close(pipe_in);
-}
-
-std::string CgiHandler::readCgiOutput(int pipe_out) {
-	std::string cgi_output;
-	char buffer[4096];
-	int bytes;
-	while ((bytes = read(pipe_out, buffer, sizeof(buffer))) > 0) {
-		cgi_output.append(buffer, bytes);
-	}
-	close(pipe_out);
-	return cgi_output;
+void CgiHandler::setupChildEnv(const std::map<std::string, std::string>& cgiParams, std::vector<char*>& env) {
+    for (const auto& param : cgiParams) {
+        std::string envVar = param.first + "=" + param.second;
+        env.push_back(strdup(envVar.c_str()));
+    }
+    env.push_back(nullptr);
 }
 
 void CgiHandler::parseCgiOutput(std::string& headers, std::string& body, int pipefd_out[2], [[maybe_unused]] int pipefd_in[2], pid_t pid) {
-	char buffer[4096];
-	int bytes;
-	struct timeval timeout;
-	timeout.tv_sec = 5;
-	timeout.tv_usec = 0;
+    char buffer[4096];
+    int bytes;
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
 
-	fd_set readfds;
-	FD_ZERO(&readfds);
-	FD_SET(pipefd_out[0], &readfds);
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(pipefd_out[0], &readfds);
 
-	std::string output;
-	while (true) {
-		fd_set tempfds = readfds;
-		int activity = select(pipefd_out[0] + 1, &tempfds, NULL, NULL, &timeout);
-		if (activity == -1) {
-			break;
-		} else if (activity == 0) {
-			Logger::red("CGI script timed out");
-			kill(pid, SIGKILL);
-			close(pipefd_out[0]);
-			return;
-		}
+    std::string output;
+    while (true) {
+        fd_set tempfds = readfds;
+        int activity = select(pipefd_out[0] + 1, &tempfds, NULL, NULL, &timeout);
+        if (activity == -1) {
+            Logger::red("Error in select while reading CGI output");
+            break;
+        } else if (activity == 0) {
+            Logger::red("CGI script timed out");
+            kill(pid, SIGKILL);
+            close(pipefd_out[0]);
+            return;
+        }
 
-		if (FD_ISSET(pipefd_out[0], &tempfds)) {
-			bytes = read(pipefd_out[0], buffer, sizeof(buffer));
-			if (bytes > 0) {
-				output.append(buffer, static_cast<size_t>(bytes));
-			} else {
-				break;
-			}
-		}
-	}
+        if (FD_ISSET(pipefd_out[0], &tempfds)) {
+            bytes = read(pipefd_out[0], buffer, sizeof(buffer));
+            if (bytes > 0) {
+                output.append(buffer, static_cast<size_t>(bytes));
+            } else {
+                break;
+            }
+        }
+    }
 
-	size_t header_end = output.find("\r\n\r\n");
-	if (header_end != std::string::npos) {
-		headers = output.substr(0, header_end);
-		body = output.substr(header_end + 4);
-	} else {
-		// No headers found
-		body = output;
-		headers = "Content-Type: text/html; charset=UTF-8";
-	}
-}
-
-
-
-bool CgiHandler::checkForRedirect(const std::string& headers) {
-	if (headers.find("Status: 302") != std::string::npos) {
-		size_t location_pos = headers.find("Location:");
-		if (location_pos != std::string::npos) {
-			size_t end_pos = headers.find("\r\n", location_pos);
-			std::string redirectLocation = headers.substr(location_pos + 9, end_pos - location_pos - 9);
-			redirectLocation.erase(0, redirectLocation.find_first_not_of(" \t"));
-			redirectLocation.erase(redirectLocation.find_last_not_of(" \t") + 1);
-
-			// Send Redirect Response
-			std::string redirect_response = "HTTP/1.1 302 Found\r\n";
-			redirect_response += "Location: " + redirectLocation + "\r\n\r\n";
-			int sent = send(client_fd, redirect_response.c_str(), redirect_response.size(), 0);
-			if (sent < 0) {
-				Logger::red("Failed to send Redirect response: " + std::string(strerror(errno)));
-			}
-			return true;
-		}
-	}
-	return false;
+    size_t header_end = output.find("\r\n\r\n");
+    if (header_end != std::string::npos) {
+        headers = output.substr(0, header_end);
+        body = output.substr(header_end + 4);
+    } else {
+        body = output;
+        headers = "Content-Type: text/html; charset=UTF-8";
+    }
 }
 
 void CgiHandler::sendCgiResponse(const std::string& headers, const std::string& body) {
-	std::string response = "HTTP/1.1 200 OK\r\n" + headers + "\r\n\r\n" + body;
-	int sent = send(client_fd, response.c_str(), response.length(), 0);
-	if (sent < 0) {
-		Logger::red("Failed to send CGI response: " + std::string(strerror(errno)));
-	}
+    std::string response = "HTTP/1.1 200 OK\r\n" + headers + "\r\n\r\n" + body;
+    int sent = send(client_fd, response.c_str(), response.length(), 0);
+    if (sent < 0) {
+        Logger::red("Failed to send CGI response: " + std::string(strerror(errno)));
+    }
 }
 
 void CgiHandler::waitForChild(pid_t pid) {
-	int status;
-	waitpid(pid, &status, 0);
+    int status;
+    waitpid(pid, &status, 0);
 }
 
 void CgiHandler::executeCGI(const std::string& cgiPath, const std::string& scriptPath,
-				const std::map<std::string, std::string>& cgiParams) {
-	int pipefd_out[2]; // For CGI stdout
-	int pipefd_in[2];  // For CGI stdin
-	if (!createPipes(pipefd_out, pipefd_in)) {
-		return;
-	}
+                            const std::map<std::string, std::string>& cgiParams) {
+    int pipefd_out[2]; // For CGI stdout
+    int pipefd_in[2];  // For CGI stdin
 
-	pid_t pid = fork();
-	if (pid < 0) {
-		Logger::red("Fork failed: " + std::string(strerror(errno)));
-		closePipe(pipefd_out);
-		closePipe(pipefd_in);
-		return;
-	}
+    if (!createPipes(pipefd_out, pipefd_in)) {
+        return;
+    }
 
-	if (pid == 0) {
-		// Child
-		runCgiChild(cgiPath, scriptPath, pipefd_out, pipefd_in, cgiParams);
-	} else {
-		// Parent
-		close(pipefd_out[1]);
-		close(pipefd_in[0]);
+    pid_t pid = fork();
+    if (pid < 0) {
+        Logger::red("Fork failed: " + std::string(strerror(errno)));
+        closePipe(pipefd_out);
+        closePipe(pipefd_in);
+        return;
+    }
 
-		writeRequestBodyIfNeeded(pipefd_in[1]);
+    if (pid == 0) {
+        // Child process
+        runCgiChild(cgiPath, scriptPath, pipefd_out, pipefd_in, cgiParams);
+    } else {
+        // Parent process
+        close(pipefd_out[1]); // Close unused write end
+        close(pipefd_in[0]);  // Close unused read end
 
-		std::string headers;
-		std::string body;
-		parseCgiOutput(headers, body, pipefd_out, pipefd_in, pid);
+        fcntl(pipefd_out[0], F_SETFL, O_NONBLOCK);
+        fcntl(pipefd_in[1], F_SETFL, O_NONBLOCK);
 
-		if (checkForRedirect(headers)) {
-			close(client_fd);
-			waitForChild(pid);
-			return;
-		}
+        int epoll_fd = epoll_create1(0);
+        if (epoll_fd == -1) {
+            Logger::red("Epoll creation failed: " + std::string(strerror(errno)));
+            closePipe(pipefd_out);
+            closePipe(pipefd_in);
+            return;
+        }
 
-		sendCgiResponse(headers, body);
+        struct epoll_event event = {};
+        struct epoll_event events[2];
 
-		close(client_fd);
-		waitForChild(pid);
-	}
+        event.events = EPOLLIN;
+        event.data.fd = pipefd_out[0];
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pipefd_out[0], &event) == -1) {
+            Logger::red("Epoll add failed: " + std::string(strerror(errno)));
+            close(epoll_fd);
+            closePipe(pipefd_out);
+            closePipe(pipefd_in);
+            return;
+        }
+
+        event.events = EPOLLOUT;
+        event.data.fd = pipefd_in[1];
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pipefd_in[1], &event) == -1) {
+            Logger::red("Epoll add failed: " + std::string(strerror(errno)));
+            close(epoll_fd);
+            closePipe(pipefd_out);
+            closePipe(pipefd_in);
+            return;
+        }
+
+        bool input_written = false;
+        std::string cgi_output;
+        char buffer[4096];
+
+        while (true) {
+            int num_events = epoll_wait(epoll_fd, events, 2, 5000); // Timeout in milliseconds
+            if (num_events == -1) {
+                Logger::red("Epoll wait failed: " + std::string(strerror(errno)));
+                break;
+            }
+
+            for (int i = 0; i < num_events; ++i) {
+                if (events[i].data.fd == pipefd_out[0] && (events[i].events & EPOLLIN)) {
+                    int bytes = read(pipefd_out[0], buffer, sizeof(buffer));
+                    if (bytes > 0) {
+                        cgi_output.append(buffer, bytes);
+                    } else {
+                        goto finish;
+                    }
+                }
+
+                if (events[i].data.fd == pipefd_in[1] && (events[i].events & EPOLLOUT)) {
+                    if (!input_written && method == "POST") {
+                        writeRequestBodyIfNeeded(pipefd_in[1]);
+                        input_written = true;
+                    }
+                }
+            }
+        }
+
+    finish:
+        close(pipefd_out[0]);
+        close(pipefd_in[1]);
+        close(epoll_fd);
+
+        std::string headers, body;
+        parseCgiOutput(headers, body, pipefd_out, pipefd_in, pid);
+
+        sendCgiResponse(headers, body);
+        waitForChild(pid);
+    }
 }
 
-void CgiHandler::closeConnection()
-{
-	activeFds.erase(client_fd);
-	serverBlockConfigs.erase(client_fd);
-	close(client_fd);
+void CgiHandler::closePipe(int fds[2]) {
+    close(fds[0]);
+    close(fds[1]);
 }
 
+void CgiHandler::writeRequestBodyIfNeeded(int pipe_in) {
+    if (method == "POST" && !requestBody.empty()) {
+        ssize_t written = write(pipe_in, requestBody.c_str(), requestBody.size());
+        if (written < 0) {
+            Logger::red("Error writing to CGI stdin: " + std::string(strerror(errno)));
+        }
+    }
+    close(pipe_in);
+}
 
+bool CgiHandler::handleCGIIfNeeded() {
+    // Prüfen, ob der Request als CGI behandelt werden muss
+    if (location->cgi.empty() || filePath.find(location->cgi) == std::string::npos) {
+        return false; // Kein CGI-Request
+    }
 
-bool CgiHandler::handleCGIIfNeeded()
-{
-	if (!location->cgi.empty()) {
-		std::string ext = getFileExtension();
-		if (filePath.size() >= ext.size() &&
-			filePath.compare(filePath.size() - ext.size(), ext.size(), ext) == 0) {
-			std::map<std::string, std::string> cgiParams;
-			cgiParams["SCRIPT_FILENAME"] = filePath;
-			cgiParams["DOCUMENT_ROOT"] = location->root.empty() ? "" : location->root;
-			cgiParams["QUERY_STRING"] = "";
-			cgiParams["REQUEST_METHOD"] = method;
-			cgiParams["SERVER_PROTOCOL"] = "HTTP/1.1";
-			cgiParams["GATEWAY_INTERFACE"] = "CGI/1.1";
-			cgiParams["SERVER_SOFTWARE"] = "my-cpp-server/1.0";
-			cgiParams["REDIRECT_STATUS"] = "200";
-			cgiParams["SERVER_NAME"] = "localhost";
-			cgiParams["SERVER_PORT"] = "8001";
-			cgiParams["REMOTE_ADDR"] = "127.0.0.1";
-			cgiParams["REQUEST_URI"] = requestedPath;
-			cgiParams["SCRIPT_NAME"] = requestedPath;
+    try {
+        std::string cgiPath = location->cgi;
+        if (cgiPath.empty()) {
+            Logger::red("No CGI path defined for this location.");
+            return false;
+        }
 
-			if (method == "POST") {
-				std::map<std::string, std::string>::const_iterator it = headersMap.find("Content-Length");
-				if (it != headersMap.end()) {
-					cgiParams["CONTENT_LENGTH"] = it->second;
-				}
-				it = headersMap.find("Content-Type");
-				if (it != headersMap.end()) {
-					cgiParams["CONTENT_TYPE"] = it->second;
-				}
-			}
-			Logger::green("test 4");
-
-			executeCGI(location->cgi, filePath, cgiParams);
-			closeConnection();
-			return true;
-		}
-	}
-	return false;
+        executeCGI(cgiPath, filePath, {{"SCRIPT_FILENAME", filePath}, {"REQUEST_METHOD", method}});
+        return true; // CGI-Request erfolgreich behandelt
+    } catch (const std::exception& e) {
+        Logger::red(std::string("CGI Error: ") + e.what());
+		errorHandler.sendErrorResponse(500, "Internal Server Error");
+    }
+    return false;
 }
--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 12:40:26 by fkeitel           #+#    #+#             */
-/*   Updated: 2024/12/12 08:43:15 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/12/13 06:52:46 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,180 +14,137 @@
 #include "../helpers/helper.hpp"
 
 #include <poll.h> // For poll
-#include <sys/epoll.h> // For epoll
+#include <sys/socket.h> // For socket functions
+#include <unistd.h> // For close
+#include <cstring> // For memset
+#include <cstdlib> // For exit
 
 int Server::create_server_socket(int port)
 {
-	int server_fd;
-	int opt; // Option value used for socket options
-	struct sockaddr_in addr; // Struct to define server address and port
+    int server_fd;
+    int opt; // Option value used for socket options
+    struct sockaddr_in addr; // Struct to define server address and port
 
-	// Create a TCP socket
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0)
-	{
-		std::perror("socket could not be created");
-		std::exit(EXIT_FAILURE);
-	}
+    // Create a TCP socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0)
+    {
+        std::perror("socket could not be created");
+        std::exit(EXIT_FAILURE);
+    }
 
-	// Set socket options to reuse address
-	opt = 1; // Enable option
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
-	{
-		std::perror("setsockopt");
-		std::exit(EXIT_FAILURE);
-	}
+    // Set socket options to reuse address
+    opt = 1; // Enable option
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        std::perror("setsockopt");
+        std::exit(EXIT_FAILURE);
+    }
 
-	// Initialize address structure to zero
-	std::memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;           // Use IPv4
-	addr.sin_addr.s_addr = INADDR_ANY;   // Accept connections on any IP address
-	addr.sin_port = htons(port);         // Set port number (network byte order)
+    // Initialize address structure to zero
+    std::memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;           // Use IPv4
+    addr.sin_addr.s_addr = INADDR_ANY;   // Accept connections on any IP address
+    addr.sin_port = htons(port);         // Set port number (network byte order)
 
-	// Bind the server socket to the address and port
-	if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-	{
-		std::perror("bind");
-		std::exit(EXIT_FAILURE);
-	}
+    // Bind the server socket to the address and port
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        std::perror("bind");
+        std::exit(EXIT_FAILURE);
+    }
 
-	// Set up the server socket to listen for incoming connections
-	if (listen(server_fd, 128) < 0)
-	{
-		std::perror("listen");
-		std::exit(EXIT_FAILURE);
-	}
+    // Set up the server socket to listen for incoming connections
+    if (listen(server_fd, 128) < 0)
+    {
+        std::perror("listen");
+        std::exit(EXIT_FAILURE);
+    }
 
-	// Set the server socket to non-blocking mode
-	setNonBlocking(server_fd);
+    // Set the server socket to non-blocking mode
+    setNonBlocking(server_fd);
 
-	return server_fd; // Return the server file descriptor
+    return server_fd; // Return the server file descriptor
 }
 
 int Server::handleServerEvent(int serv_fd, const ServerBlock& servConfig)
 {
-	// Accept the client connection
-	this->client_fd = accept(serv_fd, nullptr, nullptr);
-	if (this->client_fd < 0)
-	{
-		std::perror("accept");
-		return -1; // Signal failure
-	}
+    // Accept the client connection
+    this->client_fd = accept(serv_fd, nullptr, nullptr);
+    if (this->client_fd < 0)
+    {
+        std::perror("accept");
+        return -1; // Signal failure
+    }
 
-	// Set the client socket to non-blocking mode
-	setNonBlocking(this->client_fd);
+    // Set the client socket to non-blocking mode
+    setNonBlocking(this->client_fd);
 
-	// Add the client file descriptor to the epoll instance
-	struct epoll_event ev = {};
-	ev.events = EPOLLIN | EPOLLET; // Monitor for read events, use edge-triggered mode
-	ev.data.fd = this->client_fd;
+    // Add client FD to the active file descriptors set
+    this->activeFds.insert(this->client_fd);
 
-	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, this->client_fd, &ev) < 0)
-	{
-		std::perror("epoll_ctl (EPOLL_CTL_ADD)");
-		close(this->client_fd);
-		return -1;
-	}
+    // Map the client FD to its configuration
+    this->serverBlockConfigs[this->client_fd] = &servConfig;
 
-	// Add client FD to the active file descriptors set
-	this->activeFds.insert(this->client_fd);
-
-	// Map the client FD to its configuration
-	this->serverBlockConfigs[this->client_fd] = &servConfig;
-
-	return 0; // Success
+    return 0; // Success
 }
-
-void register_for_monitoring(const std::vector<ServerBlock>& configs,
-	struct epoll_event* changes, std::set<int>& activeFds, int epoll_fd)
-{
-	(void)changes;
-	for (const auto& conf : configs)
-	{
-		struct epoll_event ev = {};
-		ev.events = EPOLLIN; // Monitor for read events
-		ev.data.fd = conf.server_fd;
-
-		// Use the valid epoll_fd directly
-		if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, conf.server_fd, &ev) < 0)
-		{
-			std::perror("epoll_ctl");
-			std::exit(EXIT_FAILURE);
-		}
-		activeFds.insert(conf.server_fd);
-	}
-}
-
-
 
 std::vector<ServerBlock>::const_iterator Server::define_config(int fd, const std::vector<ServerBlock>& configList)
 {
-	// Iterate through the configs to find the one matching the given file descriptor
-	auto confIt = std::find_if(configList.begin(), configList.end(),
-		[fd](const ServerBlock& conf) {
-			return conf.server_fd == fd;
-		});
+    // Iterate through the configs to find the one matching the given file descriptor
+    auto confIt = std::find_if(configList.begin(), configList.end(),
+        [fd](const ServerBlock& conf) {
+            return conf.server_fd == fd;
+        });
 
-	return confIt; // Return the iterator to the matching configuration or configList.end() if not found
+    return confIt; // Return the iterator to the matching configuration or configList.end() if not found
 }
 
-void Server::process_events(int num_events)
+void Server::process_events()
 {
-	for (int i = 0; i < num_events; ++i)
-	{
-		int fd = events[i].data.fd;
-		if (events[i].events & EPOLLIN) // Check if there's data to read
-		{
-			auto confIt = define_config(fd, *configs);
-			if (confIt != configs->end())
-			{
-				handleServerEvent(fd, *confIt);
-			}
-			else
-			{
-				RequestHandler reqHan(fd, *(serverBlockConfigs[fd]),activeFds, serverBlockConfigs);
-				reqHan.handle_request();
-			}
-		}
-	}
+    // Iterate over active file descriptors
+    for (int fd : activeFds)
+    {
+        if (fd == client_fd) // Check if it is a client connection
+        {
+            auto confIt = define_config(fd, *configs);
+            if (confIt != configs->end())
+            {
+                handleServerEvent(fd, *confIt);
+            }
+            else
+            {
+                RequestHandler reqHan(fd, *(serverBlockConfigs[fd]), activeFds, serverBlockConfigs);
+                reqHan.handle_request();
+            }
+        }
+    }
 }
 
 void Server::start(const std::vector<ServerBlock>& configs_files)
 {
-	this->configs = &configs_files;
+    this->configs = &configs_files;
 
-	epoll_fd = epoll_create(1);
-	if (epoll_fd < 0)
-	{
-		std::perror("epoll_create");
-		return;
-	}
+    for (const auto& conf : *this->configs)
+    {
+        this->activeFds.insert(conf.server_fd);
+    }
 
-	register_for_monitoring(*this->configs, events, activeFds, epoll_fd);
+    while (true)
+    {
+        process_events();
+    }
 
-	while (true)
-	{
-		int num_events = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-		if (num_events < 0)
-		{
-			std::perror("epoll_wait");
-			break;
-		}
-		process_events(num_events);
-	}
-
-	close(epoll_fd);
-	close_everything();
+    close_everything();
 }
 
-// Cleans up all resources by closing active file descriptors and the kqueue instance.
+// Cleans up all resources by closing active file descriptors.
 // This ensures proper resource deallocation at the end of the program.
 void Server::close_everything(void)
 {
-	// Iterate over all active file descriptors.
-	for (int fd : activeFds)
-	{
-		close(fd); // Close each file descriptor.
-	}
-	close(epoll_fd); // Close the kqueue instance.
+    // Iterate over all active file descriptors.
+    for (int fd : activeFds)
+    {
+        close(fd); // Close each file descriptor.
+    }
 }
