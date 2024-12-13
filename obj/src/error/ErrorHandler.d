b9cc56f5128fb410d2afbf1c4bfce5f78741991e
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-12-13 10:06:33 +0100

    space

---
+++ obj/src/error/ErrorHandler.d
@@ -0,0 +1,4 @@
+obj/src/error/ErrorHandler.o: src/error/ErrorHandler.cpp \
+ src/error/./ErrorHandler.hpp src/error/./../utils/Logger.hpp
+src/error/./ErrorHandler.hpp:
+src/error/./../utils/Logger.hpp:
