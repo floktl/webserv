cb7ffd54b9cad29110e86095b5a366a98219cc84
Jonathan Eberle <jeberle@student.42heilbronn.de>
2025-01-06 12:37:37 +0100

    added client body size parsing

--- src/utils/Sanitizer.hpp
+++ src/utils/Sanitizer.hpp
@@ -6,17 +6,17 @@
 class Sanitizer {
 	private:
 		static bool isValidPath(std::string& path, const std::string& context, const std::string& pwd);
-		static long parseSize(const std::string& sizeStr, const std::string& unit);
 		static bool isValidFilename(const std::string& filename, bool allowPath);
 	public:
 		Sanitizer();
 		~Sanitizer();
+		static long parseSize(const std::string& sizeStr, const std::string& unit);
 		static bool sanitize_portNr(int portNr);
 		static bool sanitize_serverName(std::string& serverName);
 		static bool sanitize_root(std::string& root, const std::string& pwd);
 		static bool sanitize_index(std::string& index);
 		static bool sanitize_errorPage(std::string& errorPage, const std::string& pwd);
-		static bool sanitize_clMaxBodSize(std::string& clMaxBodSize);
+		static bool sanitize_clMaxBodSize(long clMaxBodSize);
 		static bool sanitize_locationPath(std::string& locationPath, const std::string& pwd);
 		static bool sanitize_locationMethods(std::string& locationMethods);
 		static bool sanitize_locationReturn(std::string& locationReturn);
@@ -24,7 +24,7 @@ class Sanitizer {
 		static bool sanitize_locationAutoindex(std::string& locationAutoindex, bool &doAutoindex);
 		static bool sanitize_locationDefaultFile(std::string& locationDefaultFile);
 		static bool sanitize_locationUploadStore(std::string& locationUploadStore, const std::string& pwd);
-		static bool sanitize_locationClMaxBodSize(std::string& locationClMaxBodSize);
+		static bool sanitize_locationClMaxBodSize(long locationClMaxBodSize);
 		static bool sanitize_locationCgi(std::string& locationCgi, std::string& locationCgiFileType, const std::string& pwd);
 		static bool sanitize_locationCgiParam(std::string& locationCgiParam);
 		static bool sanitize_locationRedirect(std::string& locationRedirect);
