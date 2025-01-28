8c6a43f5625d8c747a376f4b9bcc978fdf81fa8b
Jonathan Eberle <jeberle@student.42heilbronn.de>
2025-01-28 07:33:00 +0100

    read write problem und grober autoindex

--- src/error/ErrorHandler.cpp
+++ src/error/ErrorHandler.cpp
@@ -26,51 +26,51 @@ std::string ErrorHandler::loadErrorPage(const std::string& filePath) const
 
 std::string ErrorHandler::generateErrorResponse(Context& ctx) const
 {
-    std::ostringstream response;
-    std::string content;
+	std::ostringstream response;
+	std::string content;
 
-    Logger::file("Generating error response for error code: " + std::to_string(ctx.error_code));
+	Logger::file("Generating error response for error code: " + std::to_string(ctx.error_code));
 
-    // Find the custom error page if it exists
-    Logger::file("Searching for custom error page in errorPages map...");
-    std::map<int, std::string>::iterator it = ctx.errorPages.find(ctx.error_code);
+	// Find the custom error page if it exists
+	Logger::file("Searching for custom error page in errorPages map...");
+	std::map<int, std::string>::iterator it = ctx.errorPages.find(ctx.error_code);
 
-    if (it != ctx.errorPages.end())
-    {
-        Logger::file("Custom error page found for error code " + std::to_string(ctx.error_code) + ": " + it->second);
-        content = loadErrorPage(it->second);
-        if (content.empty())
-        {
-            Logger::file("Failed to load custom error page file: " + it->second);
-        }
-        else
-        {
-            Logger::file("Successfully loaded custom error page.");
-        }
-    }
-    else
-    {
-        Logger::file("No custom error page found for status code " + std::to_string(ctx.error_code));
-        content = "Default error page content for status " + std::to_string(ctx.error_code);
-        Logger::file("Using default error page content.");
-    }
+	if (it != ctx.errorPages.end())
+	{
+		Logger::file("Custom error page found for error code " + std::to_string(ctx.error_code) + ": " + it->second);
+		content = loadErrorPage(it->second);
+		if (content.empty())
+		{
+			Logger::file("Failed to load custom error page file: " + it->second);
+		}
+		else
+		{
+			Logger::file("Successfully loaded custom error page.");
+		}
+	}
+	else
+	{
+		Logger::file("No custom error page found for status code " + std::to_string(ctx.error_code));
+		content = "Default error page content for status " + std::to_string(ctx.error_code);
+		Logger::file("Using default error page content.");
+	}
 
-    // Fallback to default error page if content is still empty
-    if (content.empty())
-    {
-        Logger::file("Content is still empty after attempting to load custom page. Falling back to generic HTML error response.");
-        content = "<html><body><h1>" + std::to_string(ctx.error_code) + " " + ctx.error_message + "</h1></body></html>";
-    }
+	// Fallback to default error page if content is still empty
+	if (content.empty())
+	{
+		Logger::file("Content is still empty after attempting to load custom page. Falling back to generic HTML error response.");
+		content = "<!DOCTYPE html><html><head><style>body { background-color: #940000; color: white; font-family: Arial, Helvetica, sans-serif; margin: 0; padding: 0; height: 100vh; display: flex; justify-content: center; align-items: center;} h1 {font-size: 2rem;text-align: center;}</style></head><body><h1>" + std::to_string(ctx.error_code) + " " + ctx.error_message + "</h1></body></html>";
+	}
 
-    Logger::file("Preparing HTTP response headers...");
-    response << "HTTP/1.1 " << ctx.error_code << " " << ctx.error_message << "\r\n"
-             << "Content-Type: text/html\r\n"
-             << "Content-Length: " << content.size() << "\r\n\r\n"
-             << content;
+	Logger::file("Preparing HTTP response headers...");
+	response << "HTTP/1.1 " << ctx.error_code << " " << ctx.error_message << "\r\n"
+			<< "Content-Type: text/html\r\n"
+			<< "Content-Length: " << content.size() << "\r\n\r\n"
+			<< content;
 
-    Logger::file("Response successfully generated with length: " + std::to_string(content.size()));
-    Logger::file("Final response string: \n" + response.str());
+	Logger::file("Response successfully generated with length: " + std::to_string(content.size()));
+	Logger::file("Final response string: \n" + response.str());
 
-    return response.str();
+	return response.str();
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,65 +7,65 @@ std::unique_ptr<Server> serverInstance;
 
 void handle_sigint(int sig)
 {
-    (void)sig;
-    if (serverInstance)
-    {
-        Logger::cyan("\nCTRL+C received, shutting down gracefully...");
-        serverInstance->cleanup();
-        serverInstance.reset();  // Ensure cleanup
-    }
-    exit(EXIT_SUCCESS);
+	(void)sig;
+	if (serverInstance)
+	{
+		Logger::cyan("\nCTRL+C received, shutting down gracefully...");
+		serverInstance->cleanup();
+		serverInstance.reset();  // Ensure cleanup
+	}
+	exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv, char **envp)
 {
-    ConfigHandler utils;
-    GlobalFDS globalFDS;
-    serverInstance = std::make_unique<Server>(globalFDS);
-    //ClientHandler clientHandler(*serverInstance);
-    //CgiHandler cgiHandler(*serverInstance);
+	ConfigHandler utils;
+	GlobalFDS globalFDS;
+	serverInstance = std::make_unique<Server>(globalFDS);
+	//ClientHandler clientHandler(*serverInstance);
+	//CgiHandler cgiHandler(*serverInstance);
 
-    // Set up signal handling
-    struct sigaction action;
-    action.sa_handler = handle_sigint;  // Register the function
-    sigemptyset(&action.sa_mask);
-    action.sa_flags = 0;  // No special flags needed
+	// Set up signal handling
+	struct sigaction action;
+	action.sa_handler = handle_sigint;  // Register the function
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;  // No special flags needed
 	Logger::yellow("Server Starting...");
-    if (sigaction(SIGINT, &action, NULL) == -1)
-    {
-        Logger::red() << "Failed to set signal handler!";
-        return EXIT_FAILURE;
-    }
+	if (sigaction(SIGINT, &action, NULL) == -1)
+	{
+		Logger::red() << "Failed to set signal handler!";
+		return EXIT_FAILURE;
+	}
 
-    try
-    {
-        utils.parseArgs(argc, argv, envp);
-        if (!utils.getconfigFileValid())
-        {
-            Logger::red() << "Invalid configuration file!";
-            return EXIT_FAILURE;
-        }
+	try
+	{
+		utils.parseArgs(argc, argv, envp);
+		if (!utils.getconfigFileValid())
+		{
+			Logger::red() << "Invalid configuration file!";
+			return EXIT_FAILURE;
+		}
 
-        std::vector<ServerBlock> configs = utils.get_registeredServerConfs();
-        if (configs.empty())
-        {
-            Logger::red() << "No configurations found!";
-            return EXIT_FAILURE;
-        }
+		std::vector<ServerBlock> configs = utils.get_registeredServerConfs();
+		if (configs.empty())
+		{
+			Logger::red() << "No configurations found!";
+			return EXIT_FAILURE;
+		}
 
-        int epoll_fd = serverInstance->server_init(configs);
-        if (epoll_fd == EXIT_FAILURE)
-            return EXIT_FAILURE;
+		int epoll_fd = serverInstance->server_init(configs);
+		if (epoll_fd == EXIT_FAILURE)
+			return EXIT_FAILURE;
 
-        if (serverInstance->runEventLoop(epoll_fd, configs) == EXIT_FAILURE)
-            return EXIT_FAILURE;
-    }
-    catch (const std::exception &e)
-    {
-        std::string err_msg = "Error: " + std::string(e.what());
-        Logger::red() << err_msg;
-        return EXIT_FAILURE;
-    }
+		if (serverInstance->runEventLoop(epoll_fd, configs) == EXIT_FAILURE)
+			return EXIT_FAILURE;
+	}
+	catch (const std::exception &e)
+	{
+		std::string err_msg = "Error: " + std::string(e.what());
+		Logger::red() << err_msg;
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
